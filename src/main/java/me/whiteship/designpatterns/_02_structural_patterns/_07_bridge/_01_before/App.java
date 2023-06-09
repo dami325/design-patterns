cf5e75a490ac152b7ddca794e1b082a5a20e30ca
Judalm Park (dami) <wnekfa1004@naver.com>
2023-06-09 23:59:29 +0900

    에러코드 주석

--- src/main/java/me/whiteship/designpatterns/_02_structural_patterns/_07_bridge/_01_before/App.java
+++ src/main/java/me/whiteship/designpatterns/_02_structural_patterns/_07_bridge/_01_before/App.java
@@ -3,8 +3,8 @@ package me.whiteship.designpatterns._02_structural_patterns._07_bridge._01_befor
 public class App {
 
     public static void main(String[] args) {
-        Champion kda아리 = new KDA아리();
-        kda아리.skillQ();
-        kda아리.skillR();
+//        Champion kda아리 = new KDA아리();
+//        kda아리.skillQ();
+//        kda아리.skillR();
     }
 }
--- src/main/java/me/whiteship/designpatterns/_02_structural_patterns/_07_bridge/_02_after/App.java
+++ src/main/java/me/whiteship/designpatterns/_02_structural_patterns/_07_bridge/_02_after/App.java
@@ -5,12 +5,12 @@ import me.whiteship.designpatterns._02_structural_patterns._07_bridge._01_before
 public abstract class App implements Champion {
 
     public static void main(String[] args) {
-        Champion kda아리 = new 아리(new KDA());
-        kda아리.skillQ();
-        kda아리.skillW();
-
-        Champion poolParty아리 = new 아리(new PoolParty());
-        poolParty아리.skillR();
-        poolParty아리.skillW();
+//        Champion kda아리 = new 아리(new KDA());
+//        kda아리.skillQ();
+//        kda아리.skillW();
+//
+//        Champion poolParty아리 = new 아리(new PoolParty());
+//        poolParty아리.skillR();
+//        poolParty아리.skillW();
     }
 }
